caa6b52ac63b0a95ab8448d95fd58a3af495aa54
Rachel Myers <myersrse@gmail.com>
2017-12-07 11:10:12 -0800

    form registration

---
+++ registrationApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    cpassword: '',
+    street: '',
+    unit: '',
+    city: '',
+    state: '',
+    luck: null,
+  }
+}
